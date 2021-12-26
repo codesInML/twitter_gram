99b633d941f56e5bb3cdb0050e600a598bbe43be
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2021-12-26 19:54:41 +0100

    pushed to github

--- build/resources/static/assets/uploads/must_have.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-const must_have = "this file is needed here cause images are stored in this directory";
--- build/src/errors/customError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CustomError extends Error {
-    constructor(message) {
-        super(message);
-    }
-}
-exports.default = CustomError;
